f77477a3f23157d34d6488e41a1b784c249c58fb
Eric <eric.leleu@worldline.com>
2017-04-19 16:38:21 +0200

    addition of provi for benchmark

--- DWAccessPoint/src/main/java/net/wl/poc/resources/GatewayResource.java
+++ DWAccessPoint/src/main/java/net/wl/poc/resources/GatewayResource.java
@@ -2,7 +2,6 @@ package net.wl.poc.resources;
 
 import java.io.IOException;
 
-import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -10,17 +9,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.representation.EntitlementRequest;
+import org.keycloak.authorization.client.representation.PermissionRequest;
+
 import com.codahale.metrics.annotation.Timed;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 import de.ahus1.keycloak.dropwizard.User;
 import io.dropwizard.auth.Auth;
-import org.keycloak.authorization.client.AuthzClient;
-import org.keycloak.authorization.client.representation.EntitlementRequest;
-import org.keycloak.authorization.client.representation.EntitlementResponse;
-import org.keycloak.authorization.client.representation.PermissionRequest;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -31,47 +28,289 @@ public class GatewayResource {
 	@Path("y")
 	@GET
 	@Timed
-	//@RolesAllowed("gateway:service-y")
+	// @RolesAllowed({ "gateway:service-y" })
 	public String callServiceY(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
-
 		System.out.println(auth);
-		// create a new instance based on the configuration defined in keycloak-authz.json
+		checkPermissionOnResource(auth, "Y");
+		/*
+		 * Request requesty = new Request.Builder().url("http://localhost:8081/helloY").header("Authorization", auth).build(); Response response =
+		 * client.newCall(requesty).execute(); return response.body().string();
+		 */
+		return "Access Granted to Y";
+	}
+
+	private void checkPermissionOnResource(String auth, String resourceName) {
 		AuthzClient authzClient = AuthzClient.create();
-
-		// obtain an Entitlement API Token to get access to the Entitlement API.
-		// this token is an access token issued to a client on behalf of an user
-		// with a scope = kc_entitlement
-		//String eat = getEntitlementAPIToken(authzClient);
-
-		// send the entitlement request to the server to
-		// obtain an RPT with all permissions granted to the user
-		EntitlementResponse eresponse = authzClient.entitlement(auth.replaceFirst("Bearer ","")).getAll("gateway");
-		String rpt = eresponse.getRpt();
-		System.out.println("You got a RPT: " + rpt);
-
 		EntitlementRequest request = new EntitlementRequest();
 		PermissionRequest permission = new PermissionRequest();
-		permission.setResourceSetName("Y");
+		permission.setResourceSetName(resourceName);
 		request.addPermission(permission);
-		authzClient.entitlement(auth.replaceFirst("Bearer ","")).get("gateway", request);
-		rpt = eresponse.getRpt();
-		System.out.println("You got a RPT 2 : " + rpt);
-
-		Request requesty = new Request.Builder().url("http://localhost:8081/helloY").header("Authorization", auth).build();
-		Response response = client.newCall(requesty).execute();
-		return response.body().string();
+		authzClient.entitlement(auth.replaceFirst("Bearer ", "")).get("gateway", request);
 	}
 
 	@Path("x")
 	@GET
 	@Timed
-	@RolesAllowed({ "gateway:service-x" })
+	// @RolesAllowed({ "gateway:service-x" })
 	public String callServiceX(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
-
 		System.out.println(auth);
+		checkPermissionOnResource(auth, "X");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to X";
+	}
 
-		Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build();
-		Response response = client.newCall(request).execute();
-		return response.body().string();
+	@Path("a")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceA(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "A");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to A";
+	}
+
+	@Path("b")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceB(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "B");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to B";
+	}
+
+	@Path("c")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceC(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "C");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to C";
+	}
+
+	@Path("d")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceD(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "D");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to D";
+	}
+
+	@Path("e")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceE(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "E");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to E";
+	}
+
+	@Path("f")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceF(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "F");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to F";
+	}
+
+	@Path("g")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceg(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "G");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to G";
+	}
+
+	@Path("h")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceH(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "H");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to H";
+	}
+
+	@Path("i")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceI(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "I");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to I";
+	}
+
+	@Path("j")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceJ(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "J");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to J";
+	}
+
+	@Path("k")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceK(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "K");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to K";
+	}
+
+	@Path("l")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceL(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "L");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to L";
+	}
+
+	@Path("m")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceM(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "M");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to M";
+	}
+
+	@Path("n")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceN(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "N");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to N";
+	}
+
+	@Path("o")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceO(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "O");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to O";
+	}
+
+	@Path("p")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceP(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "P");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to P";
+	}
+
+	@Path("q")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceQ(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "Q");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to Q";
+	}
+
+	@Path("r")
+	@GET
+	@Timed
+	// @RolesAllowed({ "gateway:service-x" })
+	public String callServiceR(@HeaderParam("Authorization") String auth, @Auth User principal) throws IOException {
+		System.out.println(auth);
+		checkPermissionOnResource(auth, "R");
+		/*
+		 * Request request = new Request.Builder().url("http://localhost:8082/helloX").header("Authorization", auth).build(); Response response =
+		 * client.newCall(request).execute(); return response.body().string();
+		 */
+		return "Access Granted to R";
 	}
 }
--- provisioning/src/main/java/net/wl/poc/SimpleMain.java
+++ provisioning/src/main/java/net/wl/poc/SimpleMain.java
@@ -1,45 +1,118 @@
 package net.wl.poc;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleScopeResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
-import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
-import org.keycloak.representations.info.SystemInfoRepresentation;
-
-import java.util.List;
 
 /**
  * Created by eric on 13/04/17.
  */
 public class SimpleMain {
-    public static void main(String[] args) {
-        Keycloak keycloackClient = Keycloak.getInstance("http://localhost:8180/auth", "master", "admin", "admin", "admin-cli");
+	public static void main(String[] args) {
+		final String REALM = "yep-realm";
+		final String FRONT_APP_CLIENTID = "front-app";
+		final String GATEWAY_CLIENTID = "gateway";
 
-        RealmResource realm = keycloackClient.realm("my-realm");
-        UsersResource users = realm.users();
-        System.out.println("Show Users: ("+ users.count()+")");
-        System.out.println("USERS : ");
-        List<UserRepresentation> allUsers = users.search(null, null, null);
-        for (UserRepresentation user: allUsers) {
-            System.out.println("+++++++++ UserId : " + user.getId());
-            System.out.println("Login : " + user.getUsername());
-            System.out.println("Roles  : " + user.getClientRoles());
-            System.out.println("Groups  : " + user.getGroups());
+		Keycloak keycloackClient = Keycloak.getInstance("http://localhost:8180/auth", "master", "admin", "admin", "admin-cli");
+		RealmResource realm = keycloackClient.realm(REALM);
 
-            UserResource userRes = users.get(user.getId());
-            user.setFirstName(user.getFirstName() + " - " + System.currentTimeMillis());
-            System.out.println("User in " + userRes.groups().size() + " groups");
-            userRes.update(user);
-        }
+		final String LETTERS = "XY";// ABCDEFGHIJKLMNOPQR";
 
-        UserRepresentation userTest = users.search("test", null, null, null, null, null).get(0);
-        System.out.println("+++++++++ UserId (with more details?? no): " + userTest.getId());
-        System.out.println("Login : " + userTest.getUsername());
-        System.out.println("Roles  : " + userTest.getClientRoles());
-        System.out.println("Groups  : " + userTest.getGroups());
+		// ------------------
+		// FRONT-APP
+		// ------------------
+		final String frontID = realm.clients().findByClientId(FRONT_APP_CLIENTID).get(0).getId();
+		ClientResource frontAppClient = realm.clients().get(frontID);
+		// final RolesResource frontAppRoles = frontAppClient.roles();
+		// LETTERS.chars().mapToObj(i -> String.valueOf((char) i)).forEach(roleName -> {
+		// RoleRepresentation role = new RoleRepresentation();
+		// role.setClientRole(true);
+		// role.setName("front-app:page-" + roleName);
+		// frontAppRoles.create(role);
+		// });
 
-        System.out.println("Get GroupByPath : " + realm.getGroupByPath("/GroupTest").getName());
-    }
+		// ------------------
+		// GATEWAY - define role, resources, policies and permissions
+		// ------------------
+		final String gatewayId = realm.clients().findByClientId(GATEWAY_CLIENTID).get(0).getId();
+		ClientResource gatewayClient = realm.clients().get(gatewayId);
+		// final RolesResource gatewayRoles = gatewayClient.roles();
+		//
+		// final ResourcesResource resources = gatewayClient.authorization().resources();
+		// final PoliciesResource policies = gatewayClient.authorization().policies();
+		//
+		// LETTERS.chars().mapToObj(i -> String.valueOf((char) i)).forEach(roleName -> {
+		// // define new service role
+		// RoleRepresentation role = new RoleRepresentation();
+		// role.setClientRole(true);
+		// role.setName("gateway:service-" + roleName);
+		// gatewayRoles.create(role);
+		// // define the resource with a client App owner
+		// ResourceOwnerRepresentation clientOwner = new ResourceOwnerRepresentation();
+		// clientOwner.setId(gatewayId);
+		// clientOwner.setName(GATEWAY_CLIENTID);
+		// ResourceRepresentation rr = new ResourceRepresentation();
+		// rr.setName(roleName);
+		// rr.setUri("/" + roleName.toLowerCase());
+		// rr.setOwner(clientOwner);
+		// resources.create(rr);
+		// // define the policy for the resource
+		// PolicyRepresentation policy = new PolicyRepresentation();
+		// policy.setDecisionStrategy(DecisionStrategy.UNANIMOUS);
+		// policy.setLogic(Logic.POSITIVE);
+		// policy.setName("Policy-Service" + roleName);
+		// policy.setType("role");
+		// Map<String, String> config = new HashMap<>();
+		// config.put("roles", "[{\"id\":\"" + gatewayRoles.get(role.getName()).toRepresentation().getId() + "\"}]");
+		// policy.setConfig(config);
+		// policies.create(policy);
+		// // define the permission on the resource with the policy
+		// policies.policies().stream().filter((x) -> x.getName().equals(policy.getName())).map(p -> p.getId()).forEach(id -> {
+		// PolicyRepresentation permission = new PolicyRepresentation();
+		// permission.setDecisionStrategy(DecisionStrategy.UNANIMOUS);
+		// permission.setLogic(Logic.POSITIVE);
+		// permission.setName(roleName);
+		// permission.setType("resource");
+		// Map<String, String> pconfig = new HashMap<>();
+		// pconfig.put("applyPolicies", "[\"" + id + "\"]");
+		// pconfig.put("resources", "[\"" + resources.find(rr.getName(), null, null, null, null, null, 1).get(0).getId() + "\"]");
+		// permission.setConfig(pconfig);
+		// policies.create(permission);
+		// });
+		// });
+
+		// -------------
+		// USERS
+		// -------------
+		UsersResource users = realm.users();
+
+		for (int i = 1; i <= 40000; i++) {
+			UserRepresentation userRepresentation = new UserRepresentation();
+			// {enabled: true, attributes: {}, requiredActions: [], username: "USERNAME"}
+			userRepresentation.setEnabled(true);
+			userRepresentation.setUsername("loginuser" + i);
+			users.create(userRepresentation);
+			// read to obtain the User internal ID
+			userRepresentation = realm.users().search(userRepresentation.getUsername(), null, null, null, null, 1).get(0);
+			UserResource userResource = realm.users().get(userRepresentation.getId());
+			// set user password
+			// {type: "password", value: "test", temporary: false}
+			CredentialRepresentation pwd = new CredentialRepresentation();
+			pwd.setTemporary(false);
+			pwd.setType("password");
+			pwd.setValue(userRepresentation.getUsername());
+			userResource.resetPassword(pwd);
+			// set user client roles
+			// [{"id":"2014e01f-9efa-4381-9fba-548a59a65991","name":"front-app:page-X","scopeParamRequired":false,"composite":false,"clientRole":true,"containerId":"b8da557e-302f-4c61-9209-2e70b27c55b1"},{"id":"d36ec0b5-9d71-431f-b7f8-01f74ccee231","name":"front-app:page-Y","scopeParamRequired":false,"composite":false,"clientRole":true,"containerId":"b8da557e-302f-4c61-9209-2e70b27c55b1"}]
+			RoleScopeResource frontRoles = userResource.roles().clientLevel(frontID);
+			frontRoles.add(frontAppClient.roles().list());
+			RoleScopeResource gatewayRoles = userResource.roles().clientLevel(gatewayId);
+			gatewayRoles.add(gatewayClient.roles().list());
+		}
+	}
 }
